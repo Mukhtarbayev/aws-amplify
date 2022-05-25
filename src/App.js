e0a1c6dc39e9c288a369341ae247e546c9f0aad9
Marat <190103491@stu.sdu.edu.kz>
2022-05-25 17:52:11 +0600

    cancel auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
@@ -58,9 +58,9 @@ function App() {
                     ))
                 }
             </div>
-            <AmplifySignOut />
+
         </div>
     );
 }
-
-export default withAuthenticator(App);
+export default App;
+// export default withAuthenticator(App);
